8bd292931c481794d94da36d572cd9b41e485fa4
dzkyct8dnb-oss <dzkyct8dnb@privaterelay.appleid.com>
2026-09-16 17:46:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { 
+    getFirestore, 
+    collection, 
+    getDocs 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 // Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyC37Cv3EtC6BdA9ug-JSbDF9RYs5jTq0kY",
@@ -7,7 +14,9 @@ const firebaseConfig = {
     messagingSenderId: "88804538178",
     appId: "1:88804538178:web:4b3ee5b92b0c8d7c4dbadb"
 };
+const app = initializeApp(firebaseConfig);
 
+const db = getFirestore(app);
 // Your existing search function
 const searchInput =
     document.getElementById("searchInput");
@@ -38,3 +47,59 @@ if (searchInput) {
     });
 
 }
+const opportunityContainer =
+    document.getElementById("opportunityContainer");
+
+
+async function loadOpportunities() {
+
+    const querySnapshot =
+        await getDocs(collection(db, "opportunities"));
+
+
+    opportunityContainer.innerHTML = "";
+
+
+    querySnapshot.forEach((doc) => {
+
+        const opportunity = doc.data();
+
+
+        opportunityContainer.innerHTML += `
+
+        <div class="opportunity-card">
+
+            <span class="badge">
+                ${opportunity.category}
+            </span>
+
+            <h3>
+                ${opportunity.title}
+            </h3>
+
+            <p>
+                ${opportunity.description}
+            </p>
+
+            <div class="card-bottom">
+
+                <span>
+                    📍 ${opportunity.location}
+                </span>
+
+                <a href="${opportunity.link}" target="_blank">
+                    Apply →
+                </a>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+
+loadOpportunities();
